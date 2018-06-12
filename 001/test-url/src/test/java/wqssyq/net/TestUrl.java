cff65d37551d1cd7f12e7d41ed967261ff13a493
zhangchenhui <zhangchenhui@163.com>
2018-06-12 18:55:07 +0800

    URL

---
+++ 001/test-url/src/test/java/wqssyq/net/TestUrl.java
@@ -0,0 +1,25 @@
+package wqssyq.net;
+
+import java.net.URLStreamHandler;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
+@Slf4j
+public class TestUrl {
+
+  @Test
+  public void test01() {
+    URLStreamHandler handler = URL.getMyURLStreamHandler("file");
+    log.info("handler : {}", handler);
+
+    handler = URL.getMyURLStreamHandler("jar");
+    log.info("handler : {}", handler);
+  }
+
+  @Test
+  public void test02() {
+    wqssyq.net.URLStreamHandler handler = URL.getURLStreamHandler("file");
+    log.info("handler : {}", handler);
+  }
+
+}
