d2199443aedd02367d1da33cbc6f84e421c3dd04
wqssyq <wqssyq@qq.com>
2019-03-24 17:19:40 +0800

    spring aop
    spring aop

---
+++ 002/spring-boot-sample-aop/src/main/java/sample/aop/monitor/ServiceMonitor.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2012-2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package sample.aop.monitor;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class ServiceMonitor {
+
+//	@Before("execution(* sample..*Service.*(..))")
+//	@Before("execution(* sample..*Service.*(..)) || execution(* net..*.*(..))")
+//	@Before("execution(* sample..*Service.*(..)) " +
+//			"&& !execution(* sample..HelloWorld03Service.*(..))")
+//	@Before("(execution(* sample..*Service.*(..)) || execution(* com.zch.sample..*.*(..)))" +
+//			" && !execution(* sample..HelloWorld03Service.*(..))")
+	@Before("(execution(* sample..*Service.*(..)) || execution(* com.zch.sample..*.*(..)))" +
+			" && !execution(* sample..HelloWorld03Service.getHelloMessage(String))")
+	public void before(JoinPoint joinPoint) {
+//		Thread.dumpStack();
+		System.out.println("before: " + joinPoint);
+		//E:\evecom\git\spring-boot\spring-boot-samples\spring-boot-sample-aop\src\main\java\sample\aop\service\HelloWorld03Service.java
+	}
+
+	@AfterReturning("execution(* sample..*Service.*(..))")
+	public void logServiceAccess(JoinPoint joinPoint) {
+//		Thread.dumpStack();
+		System.out.println("Completed: " + joinPoint);
+	}
+
+}
